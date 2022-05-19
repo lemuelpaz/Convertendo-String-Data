4de708ed8dba1c7d605371ce8e4f69f04deb8223
lemuelpaz <lemuelpazcontato@hotmail.com>
2022-05-18 22:27:23 -0300

    Convertendo String-Data

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Convertendo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "2021-12-12";
+            DateTime Alterado = DateTime.ParseExact(data, "yyyy-MM-dd", null);
+            Console.WriteLine("\nResultado: " + Alterado);
+        }
+    }
+}
